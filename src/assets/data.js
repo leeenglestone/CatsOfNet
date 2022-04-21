ba8d26db037f888a8cfe27b6d79d53612fb98494
Lee Englestone <lee.englestone@gmail.com>
2022-04-21 09:57:07 +0100

    Cats shown, logo updated

---
+++ src/assets/data.js
@@ -0,0 +1,22 @@
+var data = [
+    {
+      "catName": "Stormtrooper",
+      "personName": "Lee Englestone",
+      "img": "/cats/le-stormtrooper.jpg",
+      "description": "'Stormy' is a cool cat, never phased by anything. Perhaps a little too laid back. Brother of Molly",
+      "github": "https://github.com/leeenglestone",
+      "instagram": "https://www.instagram.com/leeenglestone/",
+      "twitter": "https://twitter.com/leeenglestone",
+      "linkedIn": "https://www.linkedin.com/in/leeenglestone/"
+    },
+    {
+      "catName": "Molly",
+      "personName": "Lee Englestone",
+      "img": "/cats/le-molly.jpg",
+      "description": "While smaller than her brother, she is the huntress of the two and has gifted us with mice and birds. Quite a nervous cat, who we're told is a bit overweight and needs to go on a diet.",
+      "github": "https://github.com/leeenglestone",
+      "instagram": "https://www.instagram.com/leeenglestone/",
+      "twitter": "https://twitter.com/leeenglestone",
+      "linkedIn": "https://www.linkedin.com/in/leeenglestone/"
+    }
+  ];
